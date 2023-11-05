2e282c638c3aa33be7407c274825fab3d0116b7a
nikilesh <nikileshramesh2003@gmail.com>
2023-11-05 00:49:51 +0000

    ekf edit

--- ExtendedKalmanFilter.m
+++ ExtendedKalmanFilter.m
@@ -1,8 +1,12 @@
 % Written by Nikilesh Ramesh - 02/11/2023
 
 % state = [\lambda offset_vector] where \lambda = [roll {\phi}, pitch {\theta}, yaw {\psi}]
-function [state, cov] = ExtendedKalmanFilter(p_state, p_cov, data)
+function [state, P] = ExtendedKalmanFilter(p_state, p_cov, data)
 
+% data
+omega  = data.gyro; %needs to be column
+[i,~] = size(state); % YOU SURE???
+%state space calc
 sr = sin(p_state(1)); %sin roll
 tp = tan(p_state(2)); %tan pitch
 cr = cos(p_state(1)); %cos roll
@@ -15,9 +19,17 @@ B = -A;
 Z3 = zero(3,3);
 stM_r1 = [Z3 A];
 stM_r2 = [Z3 Z3];
-stM = vertcat(stM_r1, stM_r2);
+At = vertcat(stM_r1, stM_r2);
+Bt = vertcat(B,Z3);
+
+%predict
+state_dt = At*p_state + Bt*omega + K*(meas - p_meas); %WHAT IS THIS SHIT
+P = At*p_cov*At' + Q;
+%update
+K = P*H'\(H*P*H' + R);
+P = (eye(i) - K*H)*P;
 
 %
 state = 0;
-cov = 0;
+P = 0;
 end
